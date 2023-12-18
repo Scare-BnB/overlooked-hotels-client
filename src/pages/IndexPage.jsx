99ccab63c86e816e3fe1331d66339c7e09c23b16
alex-the-pigeon <xelakart@gmail.com>
2023-12-18 17:18:02 +1100

    feat: add test image to index page

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -2,12 +2,17 @@ import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import HomeHero from "../components/HomeHero";
+import GrandLodge1 from "../assets/location_images/grand_lodge1.png";
 
 
 export default function IndexPage() {
     return (
       <div className="flex flex-col min-h-screen">
       <Header />
+
+     <div>
+      <img src={GrandLodge1} alt="Home Page test" className="w-full h-auto" />
+    </div>
      
       <div className="flex flex-col min-h-screen">
       <HomeHero />
@@ -17,6 +22,7 @@ export default function IndexPage() {
       </div>
       </div>
       </div>
+      
 
     );
 }
