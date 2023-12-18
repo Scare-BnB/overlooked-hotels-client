3c73f546707177e0c567f0faab57a3e34ae5816f
alex-the-pigeon <xelakart@gmail.com>
2023-12-18 19:37:54 +1100

    update: updated BookingsPage.jsx

--- src/pages/BookingsPage.jsx
+++ src/pages/BookingsPage.jsx
@@ -2,5 +2,17 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 export default function BookingsPage() {
-    return "Bookings page will be here";
+    return (
+      <div className="flex flex-col min-h-screen">
+      <Header />
+     
+      <p>booking form will go here</p>
+      
+      <div className="flex flex-col min-h-screen md:flex justify-end translate-y-2 translate-x-0 -mb-8">
+      <Footer />
+      </div>
+      </div>
+      
+
+    );
 }
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,50 +1,24 @@
 import { Link } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import ContactForm from "../components/ContactForm";
 
 
 export default function ContactPage() {
     return (
 
-        <div className="hero max-h-screen py-64 ">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <form className="card-body">
-        <div className="form-control ">
-          <label className="label">
-            <span className="label-text">Message</span>
-          </label>
-          <input type="message" placeholder="Type message here" className="input input-bordered" required />
-        </div>
-        <div className="form-control">
+      <div className="flex flex-col min-h-screen">
+      <Header />
+
+     <ContactForm />
+     
     
-        </div>
-        <div className="form-control mt-8">
-          <button className="btn btn-primary">Submit</button>
-        </div>
-      </form>
+      
+      <div className="flex flex-col min-h-screen md:flex justify-end translate-y-1 translate-x-0 -mb-4">
+      <Footer />
       </div>
-          <div>
-            <h1 className="text-5xl font-bold">Contact Us</h1>
-            <p className="py-6">Provident cupiditate voluptatem et in.dfsfsdfsdfdsfdsfsdfsdfsdf </p>
-            <div class="flex items-center mt-5 text-gray-600 dark:text-gray-400">
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"/>
-                            </svg>
-                            <div class="ml-4 text-md tracking-wide font-semibold w-40">
-                                +44 1234567890
-                            </div>
-                        </div>
-                        <div class="flex items-center mt-2 text-gray-600 dark:text-gray-400">
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/>
-                            </svg>
-                            <div class="ml-4 text-md tracking-wide font-semibold w-40">
-                                jack@overlookedhotels.com
-                            </div>
-                        </div>
-                   
-          </div>
-        </div>
       </div>
+      
     
     );
 }
