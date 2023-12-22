2349983a006838814ae14b5c11bb6859d4dccb0b
alex-the-pigeon <xelakart@gmail.com>
2023-12-22 14:51:00 +1100

    component: ReviewCarousel.jsx

---
+++ src/components/ReviewCarousel.jsx
@@ -0,0 +1,45 @@
+import { Component } from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+
+export default function ReviewCarousel() {
+  return (
+    <div className="grid align-items-center py-12 bg-neutral text-neutral-content">
+    <Carousel
+        showThumbs={false}
+        showIndicators={false}
+        showStatus={true}
+        infiniteLoop
+        
+    
+        >
+      <div className="items-center grid-flow-col p-4 ">
+        <p className="italic">"We had a wonderful time at the Grand Lodge. Jack was gracious host and works incredibly hard. The man deserves a holiday, because 'All work and no play is no way to stay'"</p>
+        <p className="font-bold">- Shelley (Colorado)</p>
+      </div>
+      <div className="items-center grid-flow-col p-4">
+      <p className="italic">"I got injured while hiking the surrounding trails but Annie made sure I was well taken care of while my ankles recovered. Five stars"</p>
+        <p className="font-bold">- P. Sheldon (Colorado)</p>
+  
+      </div>
+      <div className="items-center grid-flow-col p-4">
+      <p className="italic">"For an inexperienced motel manager, Norman was a delight and ensured we had everything we needed. We had a leaky shower and he turned up and fixed it for us straight away"</p>
+        <p className="font-bold">- Alfred H. (Fairvale, CA)</p>
+       
+      </div>
+      <div className="items-center grid-flow-col p-4">
+      <p className="italic">"I had the best time at The Grand Lodge. The owner's son Danny is a great trail guide and took me on an amazing trike ride through the mountains"</p>
+        <p className="font-bold">- Alex K (Australia)</p>
+        </div>
+
+        <div className="items-center grid-flow-col p-4">
+      <p className="italic">"Kelsey's review goes here"</p>
+        <p className="font-bold">- Kelsey H (Australia)</p>
+       
+      </div>
+      
+    </Carousel>
+    </div>
+  );
+}
--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import TopCarousel from "../components/TopCarousel";
+import ReviewCarousel from "../components/ReviewCarousel";
 import HomeHero from "../components/HomeHero";
 import AboutHero from "../components/AboutHero";
 
@@ -24,9 +25,13 @@ export default function IndexPage() {
     <div className="flex flex-col min-h-screen">
       <AboutHero />
     
+   <div className="flex flex-col min-h-screen">
+    <ReviewCarousel />
+    
       
       <div className="flex flex-col min-h-screen md:flex justify-end translate-y-1 translate-x-0 -mb-4">
       <Footer />
+      </div>
       </div>
       </div>
       </div>
