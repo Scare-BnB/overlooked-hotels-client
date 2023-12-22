e79095a3e9a228713b7d6b155cc088a360586608
alex-the-pigeon <xelakart@gmail.com>
2023-12-22 13:05:21 +1100

    component: finished carousel

--- src/components/TopCarousel.jsx
+++ src/components/TopCarousel.jsx
@@ -1,34 +1,41 @@
+import { Component } from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+// Location images for carousel
 import GrandLodge1 from "../assets/location_images/grandlodge1.jpg";
 import GrandLodge2 from "../assets/location_images/grandlodge2.jpg";
 import Fairvale1 from "../assets/location_images/fairvale1.jpg";
 import Fairvale2 from "../assets/location_images/fairvale2.jpg";
 
-export default function() {
-    return (<div className="carousel w-full">
-  <div id="slide1" className="carousel-item relative w-full">
-    <img src={GrandLodge1}  className="w-full"/>
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-   
+export default function TopCarousel() {
+  return (
+    <div className="flex align-items-center">
+    <Carousel
+        showThumbs={false}
+        showIndicators={true}
+        showStatus={false}
+        infiniteLoop
+        autoPlay
+    
+        >
+      <div>
+        <img src={GrandLodge1} />
+       
+      </div>
+      <div>
+        <img src={Fairvale1} />
+  
+      </div>
+      <div>
+        <img src={GrandLodge2} />
+       
+      </div>
+      <div>
+        <img src={Fairvale2} />
+       
+      </div>
+    </Carousel>
     </div>
-  </div> 
-  <div id="slide2" className="carousel-item relative w-full">
-    <img src={Fairvale1} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
- 
-    </div>
-  </div> 
-  <div id="slide3" className="carousel-item relative w-full">
-    <img src={GrandLodge2} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
- 
-    </div>
-  </div> 
-  <div id="slide4" className="carousel-item relative w-full">
-    <img src={Fairvale2} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-     
-    </div>
-  </div>
-</div>
-    );
+  );
 }
