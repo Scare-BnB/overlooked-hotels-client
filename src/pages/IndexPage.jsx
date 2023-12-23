7a47c20ec3a0f90b782953a5396b1180ee854ec2
alex-the-pigeon <xelakart@gmail.com>
2023-12-23 15:23:08 +1100

    update: fixed layout issues

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,29 +1,18 @@
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 import TopCarousel from "../components/TopCarousel";
 import ReviewCarousel from "../components/ReviewCarousel";
 import HomeHero from "../components/HomeHero";
-import AboutHero from "../components/AboutHero";
+
 
 export default function IndexPage() {
   return (
     <div className="flex flex-col min-h-screen mb-5">
-
-      <div>
-        <TopCarousel />
-      </div>
+      <TopCarousel />
 
       <div className="flex flex-col min-h-screen">
         <HomeHero />
 
         <div className="flex flex-col min-h-screen">
-          <AboutHero />
-
-          <div className="flex flex-col min-h-screen">
-            <ReviewCarousel />
-
-          
-          </div>
+          <ReviewCarousel />
         </div>
       </div>
     </div>
--- src/pages/LocationsPage.jsx
+++ src/pages/LocationsPage.jsx
@@ -1,8 +1,7 @@
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 import Location1 from "../components/Location1";
 import Location2 from "../components/Location2";
 import Location3 from "../components/Location3";
+import ReviewCarousel from "../components/ReviewCarousel";
 
 export default function LocationsPage() {
   return (
@@ -14,8 +13,13 @@ export default function LocationsPage() {
 
         <div className="flex flex-col min-h-screen">
           <Location3 />
+
+          <div className="flex flex-col min-h-screen">
+          <ReviewCarousel />
         </div>
       </div>
     </div>
+    </div>
+    
   );
 }
